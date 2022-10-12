805f07b937428c77d9d21b9ee7e02619a0b889c2
Lorena Cueva <lorenacueva@gmail.com>
2022-10-12 16:00:33 -0700

    commit tree 1d0f0dc4f9dbd3b59e7c0f8e74cf05563e112b15

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if check_valid
+      if @side1 == @side2 && @side2 == @side3
+        :equilateral
+      elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  def check_valid
+    if @side1 > 0 && @side2 > 0 && @side3 > 0
+      if @side1 + @side2 > @side3 && @side1 + @side3 > @side2 && @side2 + @side3 > @side1
+        return true
+      end
+    end
+    false
+  end
+
+
+  class TriangleError < StandardError
+  end
+
 end
